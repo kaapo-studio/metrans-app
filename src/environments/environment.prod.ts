4e1be87d761c580b79d445ee6f11033ecf358f15
Kaapo <contact@kaapo.studio>
2021-05-25 15:44:58 +0300

    Angular FireBase added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDQfZTciWpQ_5pnxfO_qCPqxo3P6Gkmpe8",
+    authDomain: "metrans-38cbc.firebaseapp.com",
+    projectId: "metrans-38cbc",
+    storageBucket: "metrans-38cbc.appspot.com",
+    messagingSenderId: "953413660602",
+    appId: "1:953413660602:web:9314a0316ac95f0831f783",
+    measurementId: "G-7KQPV4W9DY"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDQfZTciWpQ_5pnxfO_qCPqxo3P6Gkmpe8",
+    authDomain: "metrans-38cbc.firebaseapp.com",
+    projectId: "metrans-38cbc",
+    storageBucket: "metrans-38cbc.appspot.com",
+    messagingSenderId: "953413660602",
+    appId: "1:953413660602:web:9314a0316ac95f0831f783",
+    measurementId: "G-7KQPV4W9DY"
+  }
 };
 
 /*
